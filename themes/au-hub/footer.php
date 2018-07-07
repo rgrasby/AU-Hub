09e742082369766f1206eaf88170f799ae7509c7
Ryan Grasby <rgrasby@gmail.com>
2018-07-06 23:02:56 -0600

    Header, footer, and front page work. Built out the general front page layout using includes.

--- themes/au-hub/footer.php
+++ themes/au-hub/footer.php
@@ -1,16 +1,21 @@
-<div class="container-fluid">
-<?php echo do_shortcode("[instagram-feed carousel=true]"); ?>     
-</div>
-		<footer class="site-footer">
-            <p id="copyright">&copy; <?php echo date('Y'); ?> Athabasca University. All rights reserved.</p>
-		</footer><!-- #colophon -->
-    </div><!-- #page -->
+        </div><!--END #main-content-->  
+            </div><!--END #content-container-->
+
+            <footer id="site-footer">
+
+                <!--Instagram Feed-->
+                <?php echo do_shortcode("[instagram-feed carousel=true]"); ?>     
+
+                <div class="sign-up">
+                    Sign up for new updates
+                </div>
+
+                <p id="copyright">&copy; <?php echo date('Y'); ?> Athabasca University. All rights reserved.</p>
+
+            </footer>
+        </div><!-- END #page -->
 
 <?php wp_footer(); ?>
-
-
-
-
 
 </body>
 </html>
--- themes/au-hub/front-page.php
+++ themes/au-hub/front-page.php
@@ -1,4 +1,35 @@
 <?php get_header(); ?>
 
+    <div class="container">
+        <section id="featured-posts">
+            <?php get_template_part( 'template-parts/content/home', 'featured' ); ?>
+        </section>
+        
+        <section id="recent-posts">
+            <?php get_template_part( 'template-parts/content/home', 'recent' ); ?>
+        </section>
+        
+        <section id="video-posts">
+            <?php get_template_part( 'template-parts/content/home', 'videos' ); ?>
+        </section>
+        <hr>
+    </div>   
+    
+    <div class="container">
+        <section id="podcast-posts">
+            <?php get_template_part( 'template-parts/content/home', 'podcasts' ); ?>  
+        </section>
+        <hr>
+        <section id="faculty-stories">
+            <?php get_template_part( 'template-parts/content/home', 'faculty' ); ?>     
+        </section>       
+    </div>
+
+    <section id="events">
+        <div class="container">
+            <h2>AU Upcoming Events</h2>
+            <?php echo do_shortcode("[2code-schedule-draw]"); ?> 
+        </div>
+    </section>
 
 <?php get_footer();
