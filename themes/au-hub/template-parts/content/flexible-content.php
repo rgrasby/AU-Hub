8e615bdd40fa59a283620eaa16f277ee3b3fa55c
Ryan Grasby <rgrasby@gmail.com>
2018-07-06 22:57:06 -0600

    Simplified single posts. Created includes for the hero banner and flexible ACF content.

--- themes/au-hub/template-parts/content/flexible-content.php
+++ themes/au-hub/template-parts/content/flexible-content.php
@@ -1,34 +1,57 @@
+<?php
+/**
+ * Template part for building out blog posts with ACF 
+ */
+?>
     <?php while ( have_posts() ) : the_post(); ?>
 
-        <div class="post-intro">
-            <div class="text-center">
-                <h1><?php echo get_the_title(); ?></h1>
-                <h2><?php echo get_field( "secondary_heading" ); ?></h2>
-            </div>
-            <?php
-            /* Using CSS :first-letter was too inconsistent. Decided to wrap first letter with a span to get dropcap effect */
-            $intro = get_field('intro');
-            $first_letter = substr($intro, 0, 1);
-            $remaining_string = substr($intro, 1);
+        <!--Attention grabbing intro text that is slightly bigger than the rest of the content--> 
+        <div id="post-intro">
+            <div class="container-sm">
+                <?php
+                /* Using CSS :first-letter was too inconsistent. Decided to wrap first letter with a span to get dropcap effect */
+                $intro = get_field('intro');
+                $first_letter = substr($intro, 0, 1);
+                $remaining_string = substr($intro, 1);
 
-            if(substr($intro, 0, 5) != '<span') {
-             $intro = '<span><span class="first-letter">'.$first_letter.'</span>'.$remaining_string;
-            }
-            if(substr($intro, -7) != '</span>') {
-             $intro .= '</span>';
-            }
-            ?>
-           <p><?php echo $intro; ?></p>
-            
+                if(substr($intro, 0, 5) != '<span') {
+                 $intro = '<span><span class="first-letter">'.$first_letter.'</span>'.$remaining_string;
+                }
+                if(substr($intro, -7) != '</span>') {
+                 $intro .= '</span>';
+                }
+                ?>
+                <p class="lead"><?php echo $intro; ?></p>
+            </div>
         </div>   
 
     <?php endwhile; ?>
 
-    <?php wp_reset_query(); ?>
+    <!-- Check if it is a video-->
+    <?php if ( has_post_format('video') ) : ?> 
 
-    <?php if ( have_rows('layout') ) : while ( have_rows('layout') ) : the_row(); ?>
+        <div class="container">
+            <div class="resp-container">
+                <?php the_field('video_embed_code'); ?>
+            </div>
+        </div>
 
-        <?php if ( get_row_layout() == 'text_with_image_and_title' ) :
+    <?php endif; ?>
+        
+    <!-- Check if it is an audio-->
+    <?php if ( has_post_format('audio') ) : ?> 
+
+        <div class="container">
+            <div class="resp-container">
+                <?php the_field('audio_embed_code'); ?>
+            </div>
+        </div>
+
+    <?php endif; ?>
+
+    <?php if ( have_rows('post_builder') ) : while ( have_rows('post_builder') ) : the_row(); ?>
+
+        <?php if ( get_row_layout() == 'content_with_image_float_and_title' ) :
 
             $image   = get_sub_field('image');
             $imgPosition = get_sub_field('image_position');
@@ -38,7 +61,7 @@
                 $float = 'img-right'; 
             endif; ?>
 
-            <div class="text-row">
+            <div class="container-sm">
                 <h2><?php the_sub_field('title'); ?></h2>
                 
                 <div class="content-with-image">
@@ -48,13 +71,15 @@
                 
             </div>
        
-        <?php elseif ( get_row_layout() == 'text' ) : ?>
+        <?php elseif ( get_row_layout() == 'content_only' ) : ?>
 
-            <div class="text-row">
-                <?php the_sub_field('content'); ?>
+            <div class="container-sm">
+                <div class="content-only">
+                    <?php the_sub_field('content'); ?>
+                </div>
             </div>
 
-        <?php elseif ( get_row_layout() == 'image' ) :
+        <?php elseif ( get_row_layout() == 'single_or_double_column_image' ) :
 
             $image   = get_sub_field('image'); 
             $image2  = get_sub_field('image_2');
@@ -62,35 +87,36 @@
             ?>
 
             <?php if ( $image ) : ?>
+                <div class="container">
+                    <div class="image-row">
+                        <div class="row">
+                            <?php if( $columns == '2'): ?>
 
-                <div class="image-row">
-                    <div class="row no-gutters">
-                        <?php if( $columns == '2'): ?>
+                                <div class="col-sm-6">
+                                    <img src="<?php echo $image['sizes']['six-by-four'] ?>" alt="<?php $image['alt'] ?>" />
+                                </div>
 
-                            <div class="col-sm-6">
-                                <img src="<?php echo $image['sizes']['six-by-four'] ?>" alt="<?php $image['alt'] ?>" />
-                            </div>
+                            <?php else: ?>
 
-                        <?php else: ?>
+                                <div class="col">
+                                    <img src="<?php echo $image['sizes']['sixteen-by-nine'] ?>" alt="<?php $image['alt'] ?>" />
+                                </div>
 
-                            <div class="col">
-                                <img src="<?php echo $image['sizes']['sixteen-by-nine'] ?>" alt="<?php $image['alt'] ?>" />
-                            </div>
+                            <?php endif; ?>
 
-                        <?php endif; ?>
+                            <?php if( $image2 and $columns == '2'): ?>
 
-                        <?php if( $image2 and $columns == '2'): ?>
+                                <div class="col-sm-6">
+                                    <img src="<?php echo $image2['sizes']['six-by-four'] ?>" alt="<?php $image2['alt'] ?>" />
+                                </div>
 
-                            <div class="col-sm-6">
-                                <img src="<?php echo $image2['sizes']['six-by-four'] ?>" alt="<?php $image2['alt'] ?>" />
-                            </div>
-
-                        <?php endif; ?>
+                            <?php endif; ?>
+                        </div>
                     </div>
                 </div>
             <?php endif; ?>
 
-
+        <!--full width quote -->
         <?php elseif ( get_row_layout() == 'quote' ) : ?>
 
             <div class="quote-row">
@@ -103,23 +129,79 @@
                 </blockquote>
             </div>
 
+        <!--Image slider -->
         <?php elseif ( get_row_layout() == 'image_slider' ) : ?>
 
             <?php 
 
-            $images = get_sub_field('gallery');
+            $images = get_sub_field('image_slider_gallery');
             $size = 'sixteen-by-nine'; 
 
             if( $images ): ?>
-                <div class="owl-carousel owl-theme">
-                    <?php foreach( $images as $image ): ?>
-                        <div class="item">
-                            <img src="<?php echo $image['sizes']['sixteen-by-nine']; ?>" alt="<?php echo $image['alt']; ?>" />
-                        </div>
-                    <?php endforeach; ?>
+                <div class="container">
+                    <div class="owl-carousel owl-theme">
+                        <?php foreach( $images as $image ): ?>
+                            <div class="item">
+                                <img src="<?php echo $image['sizes']['sixteen-by-nine']; ?>" alt="<?php echo $image['alt']; ?>" />
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
                 </div>
             <?php endif; ?>
+
+        <!--Faculty Q&A -->
+        <?php elseif ( get_row_layout() == 'faculty_q_and_a' ) : ?>
+
+            <div class="container-sm">
+                
+                <?php 
+                $photo = get_sub_field('photo');
+                $name = get_sub_field('name');
+
+                if( have_rows('q_and_a') ): while ( have_rows('q_and_a') ) : the_row(); 
+                
+                ?>
+                    <div class="qa-row">
+                        
+                        <div class="qa-wrap">
+                            <div class="qa-thumb"> 
+                                <div class="thumb">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 145"><title>Athabasca University Shield</title><path class="shield" d="M1.3 4v88.3c0 29.3 48.7 49.4 48.7 49.4s48.7-20 48.7-49.4V4H1.3zM81 92.3c0 9.7-16.5 22.5-31 29.9-14.7-7.5-31-20.1-31-29.9V21.7h62v70.6z"></path></svg>
+                                </div>
+                                <p>Athabasca University News</p>
+                            </div>
+
+                            <div class="question">
+                                <div class="inner">
+                                    <?php the_sub_field('au_question'); ?>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="qa-wrap">
+                            <div class="qa-thumb">
+                                <div class="thumb">
+                                    <?php if( $photo ): ?>
+                                        <img src="<?php echo $photo?>" alt="<?php echo $name ?>" />
+                                    <?php endif; ?>
+                                </div>
+                                <p><?php echo $name ?></p>
+                            </div>
+
+                            <div class="answer">
+                                <div class="inner">
+                                    <?php the_sub_field('faculty_answer'); ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                
+                    <?php endwhile; ?>
+                <?php endif; ?>
+            </div><!--END .container-sm-->
 
         <?php endif; ?>
 
     <?php endwhile; endif; ?>
+
+    <?php wp_reset_query(); ?>
--- themes/au-hub/template-parts/single/hero-banner.php
+++ themes/au-hub/template-parts/single/hero-banner.php
@@ -1,22 +1,44 @@
-<?php
-    $thumb_id = get_post_thumbnail_id($post->ID );
-    $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
-    $thumb_url = $thumb_url_array[0];
-?>
 
-    <!--Check if post has a featured image. If not just show title-->
-    <?php if (has_post_thumbnail( $post->ID ) ): ?>
+<!--Check if post has a featured image. If not just show title-->
+<?php if ( has_post_thumbnail() ) :
+    $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'sixteen-by-nine' ); ?>
 
-        <div id="hero">
-            <div class="hero-inner" <?php echo 'style="background-image:url('. $thumb_url. ')"'?>>
+    <div id="hero">
+        <div class="hero-bg" style="background-image:url(<?php echo esc_url( $thumbnail[0] ); ?>)"></div>
+        <div class="hero-inner">
+            <div class="container-sm">
                 <h1><?php echo the_title(); ?></h1>
+                <div class="post-meta-top">
+                    <ul class="categories">
+                        <li><?php echo get_the_category_list('<span> / </span>'); ?></li>
+                    </ul>
+                    <span>| <?php echo get_the_date(); ?></span>
+                </div>
+            </div>
+            <div class="scroll-btn">
+                <a href="#post-intro">
+                    <span class="mouse">
+                        <span></span>
+                    </span>
+                    <p>scroll down</p>
+                </a>
             </div>
         </div>
+    </div>
 
-    <?php else: ?> 
+<?php else: ?> 
 
-        <div id="title-only">
+    <div id="title-only">
+        <div class="container-sm">
             <h1><?php echo the_title(); ?></h1>
+            <div class="post-meta-top">
+                <ul class="categories">
+                    <li><?php echo get_the_category_list('<span> / </span>'); ?></li>
+                    <li><?php echo get_the_date(); ?></li>
+                </ul>
+            </div>
         </div>
+    </div>
 
-    <?php endif; ?> 
+<?php endif; ?> 
+
