75e4ecf3b564bd2636dd06b6965cf5c190fcb899
Ryan Grasby <rgrasby@gmail.com>
2018-07-13 19:19:46 -0600

    Blog articles setup and displaying in template

--- themes/au-hub/template-parts/blog/blog-articles-horizontal.php
+++ themes/au-hub/template-parts/blog/blog-articles-horizontal.php
@@ -1,21 +1,17 @@
     <article class="horizontal">
-
-        <a href="<?php the_permalink(); ?>" aria-label="Read <?php the_title(); ?>">
-
-            <?php if ( has_post_thumbnail() ) : ?>
-                <img src="<?php the_post_thumbnail_url('thumbnail'); ?>" alt="<?php the_title(); ?>" />
-            <?php endif; ?>
-
+            <div class="article-image">
+                <?php if ( has_post_thumbnail() ) : ?>
+                    <img src="<?php the_post_thumbnail_url('thumbnail'); ?>" alt="<?php the_title(); ?>" />
+                <?php endif; ?>
+            </div>
             <div class="article-content">
-                <h2><?php the_title(); ?></h2>
+                 <a href="<?php the_permalink(); ?>" aria-label="Read <?php the_title(); ?>"><h2><?php the_title(); ?></h2></a>
 
                 <div class="post-meta">
-                    <ul>
-                        <li><?php echo get_the_category_list(' , '); ?></li>
+                    <ul class="categories">
+                        <li><?php echo get_the_category_list('<span> / </span>'); ?></li>
                     </ul>
                     <p class="post-date"><?php echo get_the_date(); ?></p>
                 </div>
             </div>
-        </a>
-
     </article>
--- themes/au-hub/template-parts/blog/blog-articles.php
+++ themes/au-hub/template-parts/blog/blog-articles.php
@@ -1,26 +1,23 @@
 
 <article>
 
-    <div class="article-excerpt">
-        <a href="<?php the_permalink(); ?>" title="Read Article">
-
-            <?php if ( has_post_thumbnail() ) : ?>
-            <div class="article-image">
-                <img src="<?php the_post_thumbnail_url('large'); ?>" alt="<?php the_title(); ?>" />
-                <?php get_template_part( 'template-parts/content/format', 'icon' ); ?>
-            </div>
-            <?php endif; ?>
-
-            <h2><?php the_title(); ?></h2>
-
-        </a>
-
-        <div class="post-meta">
-            <ul>
-                <li><?php echo get_the_category_list('<span> / </span>'); ?></li>
-            </ul>
+    <?php if ( has_post_thumbnail() ) : ?>
+    <a href="<?php the_permalink(); ?>" aria-label="Read <?php the_title(); ?>">
+        <div class="article-image">
+            <img src="<?php the_post_thumbnail_url('large'); ?>" alt="<?php the_title(); ?>" />
+            <?php get_template_part( 'template-parts/content/format', 'icon' ); ?>
         </div>
+    </a>
+    <?php endif; ?>
 
+    <a href="<?php the_permalink(); ?>" aria-label="Read <?php the_title(); ?>">
+        <h2><?php the_title(); ?></h2>
+    </a>
+
+    <div class="post-meta">
+        <ul class="categories">
+            <li><?php echo get_the_category_list('<span> / </span>'); ?></li>
+        </ul>
     </div>
 
 </article>
