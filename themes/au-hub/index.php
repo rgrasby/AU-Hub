c4f450558eb1f4cfc60e2b91434a313bce644140
Ryan Grasby <rgrasby@athabascau.ca>
2018-07-20 22:27:25 -0600

    Fall back index file with came code as archive.php

--- themes/au-hub/index.php
+++ themes/au-hub/index.php
@@ -1,40 +1,32 @@
 <?php get_header(); ?>
 
     <div class="container">
-        <?php if ( have_posts() ) : ?>
-            <header class="page-header">
-                <?php
-                    the_archive_title( '<h1 class="page-title">', '</h1>' );
-                ?>
-            </header><!-- .page-header -->
-        <?php endif; ?>
         
+        <?php the_archive_title( '<h1 class="page-title">', '</h1>' );?>       
+
         <div id="articles">
 
             <div class="article-wrap row">
-
                 <?php
+                //the first two most recent events are larger
                 if ( have_posts() ) :
                     $count = 0; 
                     while (have_posts()) : the_post();
-                        $count++; 
+                    $count++; 
                     ?>
-                        <?php if ( !is_paged() ) : ?>
-                            <?php if ($count == 1) : ?>
-                                <div class="col-sm-6">					
-                                    <?php get_template_part( 'template-parts/blog/blog', 'articles' ); ?>			
-                                </div>	
-                            <?php elseif($count ==2): ?>
-                                <div class="col-sm-6">					
-                                    <?php get_template_part( 'template-parts/blog/blog', 'articles' ); ?>			
-                                </div>
-                            <?php endif; ?>
-                        <?php else: ?>
+                        <?php if ($count == 1) : ?>
+                            <div class="col-sm-6">					
+                                <?php get_template_part( 'template-parts/blog/blog', 'articles' ); ?>			
+                            </div>	
+                        <?php elseif($count == 2): ?>
+                            <div class="col-sm-6">					
+                                <?php get_template_part( 'template-parts/blog/blog', 'articles' ); ?>			
+                            </div>
+                        <?php elseif($count > 2): ?>
                             <div class="col-sm-4">					
                                 <?php get_template_part( 'template-parts/blog/blog', 'articles' ); ?>			
                             </div>		
                         <?php endif; ?>
-
                     <?php endwhile;?>
                 <?php endif; ?>
                 <?php wp_reset_postdata(); ?>
@@ -42,8 +34,8 @@
             </div>
         
             <?php if ($wp_query->max_num_pages > 1) : ?>    
-                <div class="loadmore-wrap text-center">
-                    <button id="loadmore" class="btn btn-md btn-primary"><span>Load More Posts</span></button>
+                <div class="loadmore-wrap">
+                    <button id="loadmore" class="btn btn-md btn-orange btn-inline"><span>Load More Posts</span></button>
                     <div class="the-end">
                         <h3>You reached the end.</h3>
                     </div>
@@ -54,5 +46,6 @@
 
         </div><!--END #articles -->
     </div><!--END .container -->
+
 
 <?php get_footer();?>
